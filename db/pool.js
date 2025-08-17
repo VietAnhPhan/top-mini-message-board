b3b48a09a19311ba16daee41db24f760bc92acb3
Anh Phan <vietanhphan2810@gmail.com>
2025-08-17 10:40:14 +0000

    added DATABASE_URL and used connectionString

--- db/pool.js
+++ db/pool.js
@@ -3,10 +3,14 @@ const { Pool } = require("pg");
 
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
+// module.exports = new Pool({
+//   host: process.env.HOST, // or wherever the db is hosted
+//   user: process.env.USER,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD,
+//   port: process.env.PORT, // The default port
+// });
+
 module.exports = new Pool({
-  host: process.env.HOST, // or wherever the db is hosted
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT, // The default port
+  connectionString: process.env.DATABASE_URL,
 });
